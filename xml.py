9b3b0f8042382fe2090f9f8e193e771af03a57e3
Daniel Pedrozo <daniel_pedrozo1@estudante.sesisenai.org.br>
2023-05-30 22:12:34 -0300

    x

--- xml.py
+++ xml.py
@@ -2,29 +2,32 @@ import csv
 
 caminho_tabela = 'tabela_produtos.csv'
 
-#sistema de direcionamento do usuario para sua funcao 
-def usuario():
+# Sistema de direcionamento do usuário para sua função
+def mainloop():
     while True:
-        comando = input("O que voce gostari de fazer?\n1- Adicionar novo produto\n2- Ler tabela produtos\n3- Sair\n")
+        comando = input("O que você gostaria de fazer?\n1 - Adicionar novo produto\n2 - Apagar produto existente\n3 - Ver produtos existentes\n4 - Sair\n")
         if comando == '1':
             pega_info()
-            adicionar_produto(caminho_tabela, anome, apreco, aquant)
+            adicionar_produto(anome, apreco, aquant, _id, caminho_tabela)
         elif comando == '2':
-            ler_tabela_produtos(caminho_tabela)
+            remove_produto(caminho_tabela)
         elif comando == '3':
+            ler_tabela_produtos(caminho_tabela)
+        elif comando == '4':
             break
         else:
-            print("Voce selecionou o argumento errado")
+            print("Você selecionou um argumento inválido.")
 
-#pega as informacoes do usuario para adicionar na tabela
+# Pega as informações do usuário para adicionar na tabela
 def pega_info():
-    global anome, apreco, aquant
+    global anome, apreco, aquant, _id
 
-    anome = input("Nome do produto\n")
-    apreco = input("Preco do produto\n")
-    aquant = input("Quantidade de produtos em estoque\n")
-    
-    apreco += ("R$")
+    _id = input("ID do produto em estoque: ")
+    anome = input("Nome do produto: ")
+    apreco = input("Preço do produto: ")
+    aquant = input("Quantidade de produtos em estoque: ")
+
+    apreco = "R$" + apreco
 
 # Função para ler os dados da tabela de produtos
 def ler_tabela_produtos(caminho_arquivo):
@@ -34,12 +37,38 @@ def ler_tabela_produtos(caminho_arquivo):
             print(linha)
 
 # Função para adicionar um novo produto à tabela
-def adicionar_produto(caminho_arquivo, nome, preco, quantidade):
-    novo_produto = {'Nome': nome, 'Preço': preco, 'Quantidade': quantidade}
+def adicionar_produto(nome, preco, quantidade, _id, caminho_arquivo):
+    novo_produto = {'ID': _id, 'Nome': nome, 'Preco': preco, 'Quantidade': quantidade}
     with open(caminho_arquivo, 'a', newline='') as arquivo_csv:
-        campos = ['Nome', 'Preço', 'Quantidade']
+        campos = ['ID', 'Nome', 'Preco', 'Quantidade']
         escritor = csv.DictWriter(arquivo_csv, fieldnames=campos)
+        if arquivo_csv.tell() == 0:
+            escritor.writeheader()
         escritor.writerow(novo_produto)
-    
 
-usuario()
+def remove_produto(caminho_arquivo):
+    escolha = input("Qual é o ID do produto que você gostaria de remover? ")
+    remover_produto(caminho_arquivo, escolha)
+
+def remover_produto(caminho_arquivo, escolha):
+    linhas_removidas = []
+    produtos_atualizados = []
+    with open(caminho_arquivo, 'r') as arquivo_csv:
+        leitor = csv.DictReader(arquivo_csv)
+        campos = leitor.fieldnames
+        produtos = list(leitor)
+        for produto in produtos:
+            if produto[campos[0]] == escolha:
+                linhas_removidas.append(produto)
+            else:
+                produtos_atualizados.append(produto)
+
+    with open(caminho_arquivo, 'w', newline='') as arquivo_csv:
+        campos = ['ID', 'Nome', 'Preco', 'Quantidade']
+        escritor = csv.DictWriter(arquivo_csv, fieldnames=campos)
+        escritor.writeheader()
+        for produto in produtos_atualizados:
+            escritor.writerow(produto)
+
+
+mainloop()
